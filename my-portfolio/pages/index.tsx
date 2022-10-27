d9b275ed73508b31e12d09f8f716da846eab67dc
steph javier <stephinijavi@gmail.com>
2022-10-27 12:39:44 -0700

    commit

---
+++ my-portfolio/pages/index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Steph's Portfolio</title>
+      </Head>
+      <h1 className="text-red-500 items-center">Portfolio</h1>
+    </div>
+  );
+}
