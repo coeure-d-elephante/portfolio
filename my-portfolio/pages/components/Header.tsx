cd49a9748adb49fa1e7f8193d2ebd58ffe93ea74
steph javier <stephinijavi@gmail.com>
2022-10-27 17:39:12 -0700

    commit

---
+++ my-portfolio/pages/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  );
+}
+
+export default Header;
--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/no-unescaped-entities */
 import Head from "next/head";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Steph's Portfolio</title>
+        <title>Javi's Portfolio</title>
       </Head>
-      <h1 className="text-red-500 items-center">Portfolio</h1>
+      <h1 className="p-10 text-red-500 bg-gray-50">Portfolio</h1>
+
+      <Header />
     </div>
   );
 }
